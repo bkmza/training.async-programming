97b37b8131aaaf158492058b8de70356f69ec46f
bkmza <ilya.usikov@gmail.com>
2017-10-27 08:54:40 +0300

    Added base implementation of the IFileInterface

---
+++ Training.AsyncWay/TAP/Interfaces/IFileService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.AsyncWay.TAP.Interfaces
+{
+    interface IFileService
+    {
+        /// <summary>
+        /// returns a list of all files that meet a particular search pattern, the progress callback could provide 
+        /// an estimate of the percentage of work completed as well as the current set of partial results
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        Task<ReadOnlyCollection<FileInfo>> FindFilesAsync(string pattern, IProgress<Tuple<double, ReadOnlyCollection<List<FileInfo>>>> progress);
+    }
+}
